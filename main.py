b49bf46255b6d13aa556303b431f32a25145370f
Pritam1026 <singhpritam983@gmail.comm>
2024-06-23 00:35:59 +0530

    custom_exception_defined

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
 from Insurance.logger import logging
+from Insurance.exception import InsuranceException
+import os,sys
+
 if __name__=="__main__":
     try:
-        5/0
+        3/0
     except Exception as e:
         logging.info(e)
+        raise InsuranceException(e,sys)
