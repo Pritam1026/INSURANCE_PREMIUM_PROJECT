b12fe3a158dc465d95a4c96a0352b1bd9281abf7
Pritam1026 <singhpritam983@gmail.comm>
2024-07-04 00:29:10 +0530

    config and artifact

---
+++ Insurance/entity/config_entity.py
@@ -0,0 +1,36 @@
+import os,sys
+from datetime import datetime
+from Insurance.exception import InsuranceException
+from Insurance.logger import logging
+
+FILE_NAME="insurance.csv"
+TRAIN_FILE_NAME="train.csv"
+TEST_FILE_NAME="test.csv"
+
+
+class TrainingpipelineConfig:
+    def __init__(self):
+        try:
+            self.artifact_dir=os.path.join(os.getcwd(),'artifact',f"{datetime.now().strftime("%m%d%Y-%H%M%S")}")
+        except Exception as e:
+            raise InsuranceException(e,sys)
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingpipelineConfig):
+        try:
+            self.database_name="INSURANCE" 
+            self.collection_name="INSURANCE_PROJECT"
+            self.data_ingestion_dir=os.path.join(training_pipeline_config.artifact_dir,"data_ingestion")
+            self.feature_store_file_path=os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.train_file_path=os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
+            self.test_file_path=os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
+        except Exception as e:
+            raise InsuranceException(e,sys)
+        
+    #Convert data to dict
+
+    def data_to_dict(self,)->dict:
+        try:
+            return self.__dict__
+        except Exception as e:
+            raise InsuranceException(e,sys)
--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from Insurance.logger import logging
 from Insurance.exception import InsuranceException
 import os,sys
 from Insurance.utils import get_collection_as_dataframe
+from Insurance.entity.config_entity import TrainingpipelineConfig,DataIngestionConfig
 
 if __name__=="__main__":
     try:
-        get_collection_as_dataframe()
+        #get_collection_as_dataframe()
+        training_pipeline_config=TrainingpipelineConfig()
+        data_ingestion_config=DataIngestionConfig(training_pipeline_config)
+        print(data_ingestion_config.data_to_dict())
     except Exception as e:
         logging.info(e)
         raise InsuranceException(e,sys)
